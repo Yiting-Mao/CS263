830c0fb172197f67a3c7490aa12682725da80877
Yiting-Mao <rachelym93@gmail.com>
2016-02-02 14:44:11 -0800

    Add Datastore Resource

---
+++ exchangebook/exchangebook/src/main/java/exchangebook/DatastoreResource.java
@@ -0,0 +1,13 @@
+package exchangebook;
+import exchangebook.Book;
+
+import java.io.*;
+import java.util.*;
+
+import javax.servlet.http.*;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+
+import java.util.logging.*;
+import com.google.appengine.api.datastore.*;
+import com.google.appengine.api.memcache.*;
